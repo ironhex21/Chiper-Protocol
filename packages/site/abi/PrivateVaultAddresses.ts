03f050a45457b2559963571ae0d06e9415b244d9
ironhex21 <127381417+ironhex2121@users.noreply.github.com>
2025-10-27 10:40:10 +0700

    chore: update contract address

--- packages/site/abi/PrivateVaultAddresses.ts
+++ packages/site/abi/PrivateVaultAddresses.ts
@@ -4,6 +4,6 @@
   By commands: 'npx hardhat deploy' or 'npx hardhat node'
 */
 export const PrivateVaultAddresses = { 
-  "11155111": { address: "0xF0653052e73aDce6ea08d2396Ed14270817093e1", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0x4d203c455E9D502C9a384361dAE30AE3d325953f", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" },
 };
