df8ff61803a41a76666b2789dc74a29065ae921c
ironhex21 <127381417+ironhex2121@users.noreply.github.com>
2025-11-05 00:30:55 +0700

    refactor: remove PrivateVaultAddresses in favor of ChiperProtocolAddresses

--- packages/site/abi/PrivateVaultAddresses.ts
+++
@@ -1,9 +0,0 @@
-
-/*
-  This file is auto-generated.
-  By commands: 'npx hardhat deploy' or 'npx hardhat node'
-*/
-export const PrivateVaultAddresses = { 
-  "11155111": { address: "0x4d203c455E9D502C9a384361dAE30AE3d325953f", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" },
-};
